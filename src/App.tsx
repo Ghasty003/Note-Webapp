37c0f475a7764d87cc27bc3fc31e3a2454e90d6f
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-21 02:29:14 -0400

    Added Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import './App.css'
+import Register from "./pages/Register";
 
 const App: React.FC = () => {
 
   return (
     <div className="">
-      
+      <Routes>
+        <Route path="/">
+          <Route index />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
