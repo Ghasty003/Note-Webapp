0c949a687d55ba9fe5207769a044ced939f425a2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-21 02:57:18 -0400

    Added firebase

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+interface Config {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+}
+
+const firebaseConfig: Config = {
+    apiKey: "AIzaSyBL-rCTUsXp7GdXLdxDaWwK_pSIXN77Pp4",
+    authDomain: "note-webapp-168fe.firebaseapp.com",
+    projectId: "note-webapp-168fe",
+    storageBucket: "note-webapp-168fe.appspot.com",
+    messagingSenderId: "967937720995",
+    appId: "1:967937720995:web:c397c6a685644356b47516"
+};
+
+initializeApp(firebaseConfig);
