e51d084168183655a12738aea23f64f47c3cac95
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-21 23:42:13 -0400

    Updated

--- src/pages/AddNote.tsx
+++ src/pages/AddNote.tsx
@@ -1,10 +1,25 @@
+import React from "react";
+
 const AddNote: React.FC = () => {
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
     return (
-        <form>
-            <div>
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" />
+        <form onSubmit={handleSubmit} className="bg-primary h-fit p-10 w-[500px] absolute left-[50%] translate-x-[-50%] top-[20%]
+            rounded-2xl shadow-xl">
+            <div className="flex flex-col gap-2">
+                <label className="text-xl" htmlFor="title">Title: </label>
+                <input className="w-[300px] rounded-xl outline-none px-3 py-1" type="text" id="title" />
             </div>
+
+            <div className="flex flex-col gap-2 my-10">
+                <label className="text-xl" htmlFor="content">Content: </label>
+                <textarea className="w-[300px] rounded-xl outline-none px-3 py-5" id="content" />
+            </div>
+
+            <button className="absolute right-12 bg-blue-500 p-2 bottom-4 rounded-lg drop-shadow-2xl">Save</button>
         </form>
     )
 }
