b92d60d6ac21788acc72c936d58ad1604651f8b2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2022-12-22 00:30:25 -0400

    Updated

---
+++ src/context/NoteContext.jsx
@@ -0,0 +1,34 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { createContext, useEffect, useContext } from "react";
+import { db } from "../../firebase";
+import AuthContext from "./AuthContext";
+
+const NoteContext = createContext();
+
+export function NoteContextProvider({ children }) {
+
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect(() => {
+       const getNotes = () => {
+            const unsub = onSnapshot(doc(db, "usersNote", currentUser.uid), snapshot => {
+                console.log(snapshot.data().notes)
+            })
+
+            return () => {
+                unsub();
+            }
+       }
+
+       currentUser.uid && getNotes();
+    }, [currentUser.uid]);
+
+    return (
+        <NoteContext.Provider value={{}}>
+            { children }
+        </NoteContext.Provider>
+    )
+}
+
+
+export default NoteContext;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { NoteContextProvider } from './context/NoteContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AuthContextProvider>
-    <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    <NoteContextProvider>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </NoteContextProvider>
   </AuthContextProvider>,
 )
